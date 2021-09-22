a25e484b06bf71a4da0629713c5cc5abd33ff65b
Anatolii Tumakov <ajzava.shota@mail.ru>
2021-09-22 20:19:30 +0300

    trabl

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace ДЗ_2_v0._1
+{
+	public class Animal
+	{
+		public string Name { get; set; }
+		public string View { get; set; }
+		public string Classs { get; set; }
+
+		public Animal(string Name, string View, string Classs)
+		{
+			Name = name;
+			View = view;
+			Classs = classs;
+		}
+
+		virtual public void Voice()
+		{
+			Console.WriteLine("Рычит");
+		}
+
+		virtual public void Food()
+		{
+			Console.WriteLine("Ем");
+		}
+
+		virtual public void Eyes()
+		{
+			Console.WriteLine("Вертикальные");
+		}
+
+		public void Going()
+		{
+			Console.WriteLine("Я умею ходить");
+		}
+
+		public void Fun()
+		{
+			Console.WriteLine("умею резвиться")
+	
+		}
+
+		virtual public void Hunting()
+		{
+			Console.WriteLine("я умею охотиться")
+	
+		}
+
+	}
+}	
---
+++ Class2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Goat 
+
+namespace ДЗ_2_v0._1
+{
+    class Goat : Animal
+    { public string Postion { get; set; }      
+     
+        public override void Voice()
+        {
+            Console.WriteLine("я блею");
+        }
+
+        public override void Food()
+        {
+            Console.WriteLine("я ем марихуану");
+        }
+
+        public override void Eyes()
+        {
+            Console.WriteLine("мои зрачки вертикальные");
+        }
+
+        public override void Hunting()
+        {
+            Console.WriteLine("я не умею охотиться");
+            
+        }
+        
+    }
+
+}
